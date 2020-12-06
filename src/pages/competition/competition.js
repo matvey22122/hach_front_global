f2bee3d7780ab235389abd2eebeff58d5ce492c1
matvey22122 <matvey-03@list.ru>
2020-12-06 08:55:41 +0500

    upd

--- src/pages/competition/competition.js
+++ src/pages/competition/competition.js
@@ -1,6 +1,8 @@
 import React, {useContext, useEffect} from 'react'
 import {useHistory} from 'react-router-dom'
 import {ApiContext} from "../../context/ApiState";
+import {Button} from "antd";
+import axios from "axios";
 
 const container = {
     height: '100vh'
@@ -10,16 +12,26 @@ const Competition = () => {
     const {userLogin} = useContext(ApiContext)
     const history = useHistory()
 
-    useEffect(() => {
+    // useEffect(() => {
+    //
+    //     if (userLogin === '') {
+    //         history.goBack()
+    //     }
+    // })
 
-        if (userLogin === '') {
-            history.goBack()
-        }
-    })
+  const down = async () => {
+      const res = await axios.get(`${process.env.REACT_APP_LOCAL_URL}/secretary/download`)
+    const v = window.open("about:blank", "", "_blank");
+      v.document.write(res.data)
+  }
+
+  const start = async () => {
+      const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/secretary/start_competition`)
+  }
 
     return (
         <div style={container}>
-            <h5>OK</h5>
+            <Button onClick={() => down()}>Загрузить срезультаты соревнования</Button>
         </div>
     )
 }
